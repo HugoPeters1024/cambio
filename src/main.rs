96e44b7fb0ebdf4e03bde0f374ac1ac959e95a5e
HugoPeters1024 <hugopeters1024@gmail.com>
2025-08-30 23:14:44 +0200

    rebuild the repository based on a github template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use bevy::asset::AssetMetaCheck;
 use bevy::prelude::*;
 use bevy::window::WindowResolution;
 use bevy::winit::UpdateMode;
@@ -52,18 +53,28 @@ fn main() {
             app.insert_resource(Time::<Fixed>::from_hz(300.0));
         }
     } else {
-        app.add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "float_me_pls".to_string(),
-                resolution: WindowResolution::new(800.0, 600.0),
-                // fill the entire browser window
-                fit_canvas_to_parent: true,
-                // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
-                prevent_default_event_handling: false,
-                ..default()
-            }),
-            ..default()
-        }));
+        app.add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "float_me_pls".to_string(),
+                        resolution: WindowResolution::new(800.0, 600.0),
+                        // Bind to canvas included in `index.html`
+                        canvas: Some("#bevy".to_owned()),
+                        // fill the entire browser window
+                        fit_canvas_to_parent: true,
+                        // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
+                        prevent_default_event_handling: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(AssetPlugin {
+                    // Breaks in the browser from some reason otherwise
+                    meta_check: AssetMetaCheck::Never,
+                    ..default()
+                }),
+        );
 
         // Because we doing networking, there might be updates to the game
         // even when the window is not in focus, so we should ensure our update
@@ -77,7 +88,32 @@ fn main() {
         app.add_plugins(ClientPlugin);
 
         app.add_plugins((GameAssetPlugin, CardPlugin));
+
+        app.add_systems(Startup, set_window_icon);
     }
 
     app.run();
+}
+
+// Sets the icon on windows and X11
+fn set_window_icon(
+    windows: NonSend<WinitWindows>,
+    primary_window: Query<Entity, With<PrimaryWindow>>,
+) -> Result {
+    let primary_entity = primary_window.single()?;
+    let Some(primary) = windows.get_window(primary_entity) else {
+        return Err(BevyError::from("No primary window!"));
+    };
+    let icon_buf = Cursor::new(include_bytes!(
+        "../build/macos/AppIcon.iconset/icon_256x256.png"
+    ));
+    if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
+        let image = image.into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        let icon = Icon::from_rgba(rgba, width, height).unwrap();
+        primary.set_window_icon(Some(icon));
+    };
+
+    Ok(())
 }
